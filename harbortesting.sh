bac1fe1da07e71309921614a407155f457989b97
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2023-01-10 15:24:58 +0530

    Update harbortesting.sh

--- harbortesting.sh
+++ harbortesting.sh
@@ -1,7 +1,10 @@
 cd /home/iplon/repos_2
 
 
-mkdir -p /etc/docker/certs.d/ivpn.iplon.co.in:8443
+docker pull ivpn.iplon.co.in:8443/testing/rabbitmq@sha256:6982b720f02077d2675a61c512b125cc6e810d0fae5a010c66981a393eeac8c7
+
+
+
 
 
 
